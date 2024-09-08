e805a3ed99a80e196440a94ffdeff88610a48ed4
Tommy AKONGUINA <tommy.akonguina@yahoo.fr>
2024-09-08 13:32:33 +0200

    add route comics

--- routes/comics.js
+++ routes/comics.js
@@ -2,6 +2,21 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  try {
+    const { title, skip } = req.body;
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${
+        process.env.API_KEY
+      }${title ? `&title=${title}` : ""}${skip ? `&skip=${skip}` : ""}`
+    );
+    console.log(response);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get("/:characterId", async (req, res) => {
   try {
     const { characterId } = req.params;
